f01f33ee7c92b265690906adc887dc674ec1fc01
Jos Visser <1911646+Qballjos@users.noreply.github.com>
2025-04-10 14:13:45 +0200

    add wordpress build when folder empty

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
+# Download WordPress als het nog niet bestaat
+if [ ! -f /var/www/html/wp-config.php ]; then
+  echo "WordPress not found - downloading..."
+  curl -o /tmp/wordpress.tar.gz https://wordpress.org/latest.tar.gz
+  tar -xzf /tmp/wordpress.tar.gz -C /var/www/html --strip-components=1
+  chown -R www-data:www-data /var/www/html
+fi
+
 # Start Redis
 service redis-server start
 
-# Start Apache in foreground
+# Start Apache
 apachectl -D FOREGROUND
